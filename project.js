88dc4d1924e7c15aa2c5558e3acc9a4c64107f2a
nyrellcl <nyrell.leonor2000@gmail.com>
2022-11-01 16:36:45 -0700

    added myteam project

--- project.js
+++ project.js
@@ -6,9 +6,9 @@ const swiper = new Swiper('.swiper', {
     coverFlowEffect:{
         rotate: 50,
         stretch: 0,
-        depth: 200, 
+        depth: 100, 
         modifier: 1,
-        slideShadows: true
+        slideShadows: false
     },
     direction: 'horizontal',
     loop: true,
